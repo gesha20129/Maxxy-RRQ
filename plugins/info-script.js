c298b5462081f85026effab84440a693a19fe310
gesha20129 <91309083+gesha20129@users.noreply.github.com>
2022-10-09 23:08:16 +0700

    Update info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -8,8 +8,8 @@ let str = `*https://wa.me/6285891318105`
 let wibu = `https://hadi-api.herokuapp.com/api/loli` 
 let thumb = await(await fetch(wibu)).buffer()
 conn.sendButtonDoc(m.chat, str, wm,'ᴏᴋ','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://youtu.be/lqa4vJIXTi8",
-    title: 'https://youtu.be/lqa4vJIXTi8',
+    mediaUrl: "https://youtube.com/channel/UCWgc-_sSNyOGqvgY2RxR1YA",
+    title: 'https://youtube.com/channel/UCWgc-_sSNyOGqvgY2RxR1YA',
     body: 'want source code?',
     thumbnail: thumb,
     sourceUrl: sig
